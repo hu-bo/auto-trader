ca88173b21f27cc2f0d983b4e039bfdcf7530436
hubo02 <hubo02@bilibili.com>
2026-01-28 23:44:14 +0800

    proto

--- apps/exchange-service/src/grpc/server.ts
+++ apps/exchange-service/src/grpc/server.ts
@@ -11,7 +11,7 @@ const logger = createLogger('grpc-server');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PROTO_PATH = join(__dirname, '../../proto/exchange.proto');
+const PROTO_PATH = join(__dirname, '../../../../packages/contracts/proto/exchange.proto');
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: false,
