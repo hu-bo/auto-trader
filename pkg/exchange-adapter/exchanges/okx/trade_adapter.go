1c24e32db1dffcacaff21b32c21cf381167cb363
hubo02 <hubo02@bilibili.com>
2026-03-11 22:30:35 +0800

    111

--- pkg/exchange-adapter/exchanges/okx/trade_adapter.go
+++ pkg/exchange-adapter/exchanges/okx/trade_adapter.go
@@ -744,8 +744,8 @@ func (a *TradeAdapter) PlaceStrategyOrder(ctx context.Context, params core.Strat
 		OrdType: toRawStrategyOrderType(params.StrategyType),
 		Sz:      strconv.FormatFloat(params.Quantity, 'f', -1, 64),
 	}
-	req.TgtCcy = "base_ccy"
-	fmt.Println("req", req.Sz)
+	// req.TgtCcy = "base_ccy"
+	// fmt.Println("req", req.Sz)
 	if params.TradeType != core.TradeTypeSpot && params.PositionSide != nil {
 		req.PosSide = string(*params.PositionSide)
 	}
